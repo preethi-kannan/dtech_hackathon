1e9fc9026c06ee5418b8ddb88be398d5f1ef7447
Carolina <clc113@duke.edu>
2020-07-18 17:15:07 -0400

    working with clicks for links

--- Popup.js
+++ Popup.js
@@ -1,3 +1,10 @@
-document.getElementById('btn').addEventListener("click", function(){
-	document.getElementById('body').innerHTML = "<h1 class='h1'>We are!</h1>";
-})
+// document.getElementById('btn').addEventListener("click", function(){
+// 	document.getElementById('body').innerHTML = "<h1 class='h1'>We are!</h1>";
+// })
+
+document.querySelectorAll('INPUT[Type="button"]').forEach(button =>
+	button.addEventListener('click', event => {
+        chrome.tabs.create({url: event.target.dataset.location})
+	    // window.location.href = event.target.dataset.location
+	})
+  )
