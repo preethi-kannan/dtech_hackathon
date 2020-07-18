633454e207066fddfa3532e219e6f6b38c622246
Adesemi Elumaro <adesemi.elumaro@duke.edu>
2020-07-18 16:24:57 -0400

    made styling changes

--- Popup.js
+++ Popup.js
@@ -1,3 +1,3 @@
 document.getElementById('btn').addEventListener("click", function(){
-	document.getElementById('body').innerHTML = "<h1 class='h1'>We are!</h1>";
+	document.getElementById('body').innerHTML = "<h1 class='h1'>Product's amazon page/Brand's website</h1>";
 })
